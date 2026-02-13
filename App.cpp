0d9bbc1c56a838bc41c24475491e6b546eaac455
zack337 <76924414+zack337@users.noreply.github.com>
2026-02-12 23:06:16 -0800

    Size and Name

---
+++ App.cpp
@@ -0,0 +1,15 @@
+#include "App.h"
+#include <wx/wx.h>
+#include "MainFrame.h"
+
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit() 
+{
+	MainFrame* mainFrame = new MainFrame("Shauns GUI");
+	mainFrame->SetClientSize(800, 600);
+	mainFrame->Center();
+	mainFrame->Show();
+	return true;
+}
