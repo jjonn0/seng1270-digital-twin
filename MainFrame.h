b3e2505146ad9e61f3e3f459a557d6907a230ba0
zack337 <76924414+zack337@users.noreply.github.com>
2026-03-10 17:53:22 -0700

    Add event handler declarations to MainFrame class
    Created events for different uses like Sliders and Buttons that can output responses when used

--- MainFrame.h
+++ MainFrame.h
@@ -5,5 +5,12 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+private:
+	void OnAnyButtonClicked	(wxCommandEvent& evt);
+	void OnButton1Clicked(wxCommandEvent& evt);
+	void OnButton2Clicked(wxCommandEvent& evt);
+	void OnSliderChanged(wxCommandEvent& evt);
+	void OnTextChanged(wxCommandEvent& evt);
+	void OnClose(wxCloseEvent& evt);
 };
 
