7cf56e11091d58553eb604312a0a89cf84f5d822
zack337 <76924414+zack337@users.noreply.github.com>
2026-03-24 18:05:11 -0700

    Add new members and event handlers to MainFrame

--- MainFrame.h
+++ MainFrame.h
@@ -1,16 +1,31 @@
 #pragma once
 #include<wx/wx.h>
+#include <wx/Timer.h>
+#include <wx/simplebook.h>
+#include <vector>
+#include "ProfilePage.h"
+#include "StatusPage.h"
+#include "profiles.h"
+
 
 class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnAnyButtonClicked	(wxCommandEvent& evt);
-	void OnButton1Clicked(wxCommandEvent& evt);
-	void OnButton2Clicked(wxCommandEvent& evt);
-	void OnSliderChanged(wxCommandEvent& evt);
-	void OnTextChanged(wxCommandEvent& evt);
-	void OnClose(wxCloseEvent& evt);
+	wxTimer m_timer;
+	wxSimplebook* pageContainer;
+	wxPanel* page1;
+	ProfilePage* page2;
+	StatusPage* page3;
+	wxStaticText* m_timeText;
+	PatientProfile* m_currentPatient;
+	StaffProfile* m_currentStaff;
+	void OnTimer(wxTimerEvent& evt);
+	void OnKeyEvent(wxKeyEvent& evt);
+	std::vector<PatientProfile> m_patient;
+	int m_Index = 0;
+
+	wxDECLARE_EVENT_TABLE();
 };
 
