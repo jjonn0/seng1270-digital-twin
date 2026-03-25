e6a0d37a483250ff3c08bcfd515cb0e0299a7baf
zack337 <76924414+zack337@users.noreply.github.com>
2026-03-24 18:05:34 -0700

    Refactor MainFrame to improve structure and readability

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,91 +1,89 @@
-#pragma once
-#include<wx/wx.h>
-#include<vector>
-#include <wx/notebook.h>
-#include <wx/simplebook.h>
+#include "MainFrame.h"
+#include "ProfilePage.h"
+#include "StatusPage.h"
 #include "simulation.h"
 #include "profiles.h"
+#include <wx/notebook.h>
+#include <wx/simplebook.h>
+#include <format>
+#include<wx/wx.h>
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+EVT_TIMER(wxID_ANY, MainFrame::OnTimer)
+wxEND_EVENT_TABLE();
 
 
-class ProfilePage : public wxPanel
-{
-public:
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+	wxSimplebook* pageContainer = new wxSimplebook(this, wxID_ANY);
+	page1 = new wxPanel(pageContainer);
+	page2 = new ProfilePage(pageContainer);
+	page3 = new StatusPage(pageContainer);
+
+	m_patient.push_back(PatientProfile(101, "Jim", "Davis", 100, "Ate Too Much Lasanga"));
+	m_patient.push_back(PatientProfile(102, "Jerry", "Slime", 100, "Died Probably idk"));
+	m_patient.push_back(PatientProfile(103, "Laurel", "Yanny", 100, "Every word sounds the same to them."));
+	m_currentStaff = new StaffProfile(501, "Shaun", "Admin", 0, "Head of Medicine", 5000);
+
+	wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer* page1Sizer = new wxBoxSizer(wxVERTICAL);
 
 
-	ProfilePage(wxWindow* Parent) : wxPanel(Parent) {
-		InitComps();
-		Styles();
-
-	};
-	void InitComps() {
-
-		m_patSizer = new wxBoxSizer(wxVERTICAL);
-		m_profSizer = new wxBoxSizer(wxVERTICAL);
-		m_trSizer = new wxBoxSizer(wxHORIZONTAL);
-
-		m_notebook1 = new wxNotebook(this, wxID_ANY, wxPoint(200, 50), wxSize(400, 300));
-
-		m_subPanel1 = new wxPanel(m_notebook1);
-		m_subPanel2 = new wxPanel(m_notebook1);
-
-		m_staffShifts = new wxStaticText(m_subPanel2, wxID_ANY, "Loading...");
-		m_patientLabel = new wxStaticText(m_subPanel1, wxID_ANY, "Loading...");
-
-		wxButton* homeBtn = new wxButton(m_subPanel1, wxID_ANY, "Home");
-
-		homeBtn->Bind(wxEVT_BUTTON, [=](wxCommandEvent&) { static_cast<wxSimplebook*>(this->GetParent())->SetSelection(0); });
-
-		m_trSizer->Add(m_patientLabel, 0, wxALL, 10);
-		m_trSizer->AddStretchSpacer(2.1);
-		m_trSizer->Add(homeBtn, 0, wxALIGN_CENTER_VERTICAL | wxALL, 10);
-		m_trSizer->AddStretchSpacer(4);
 
 
-		m_patSizer->Add(m_trSizer, 0, wxEXPAND);
-		m_profSizer->Add(m_notebook1, 1, wxEXPAND | wxALL, 0);
-		m_subPanel1->SetSizer(m_patSizer);
-		this->SetSizer(m_profSizer);
+
+
+
+
+	page1->SetBackgroundColour(*wxLIGHT_GREY);
+	wxStaticText* HomeT = new wxStaticText(page1, wxID_ANY, "Vitals Vitals");
+	HomeT->SetFont(wxFont(22, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+	wxButton* Prof = new wxButton(page1, wxID_ANY, "Profiles");
+	wxButton* Stat = new wxButton(page1, wxID_ANY, "Status");
+
+
+
+
+
+
+
+	wxStaticText* StatT1 = new wxStaticText(page3, wxID_ANY, "Third Page");
+
+
+	page1Sizer->Add(HomeT, 0, wxALIGN_CENTER | wxTOP, 10);
+	page1Sizer->AddSpacer(40);
+	page1Sizer->Add(Prof, 0, wxALIGN_CENTER | wxALL, 10);
+	page1Sizer->Add(Stat, 0, wxALIGN_CENTER | wxALL, 10);
+	page1->SetSizer(page1Sizer);
+
+
+
+	pageContainer->AddPage(page1, "Home");
+	pageContainer->AddPage(page2, "Profile Page");
+	pageContainer->AddPage(page3, "Status");
+
+	mainSizer->Add(pageContainer, 1, wxEXPAND);
+	this->SetSizer(mainSizer);
+
+	this->Layout();
+
+	Prof->Bind(wxEVT_BUTTON, [=](wxCommandEvent&) { pageContainer->SetSelection(1); });
+	Stat->Bind(wxEVT_BUTTON, [=](wxCommandEvent&) { pageContainer->SetSelection(2); });
+
+	m_timer.SetOwner(this);
+	m_timer.Start(1000);
+
+
+	CreateStatusBar();
+
+}
+
+void MainFrame::OnTimer(wxTimerEvent& evt) {
+	if (m_Index >= m_patient.size()) {
+		m_Index = 0;
 	}
-	void Styles() {
-		this->SetBackgroundColour(*wxLIGHT_GREY);
-		m_subPanel1->SetBackgroundColour(*wxLIGHT_GREY);
-		m_notebook1->AddPage(m_subPanel1, "Patient Profiles");
-
-		m_subPanel2->SetBackgroundColour(*wxLIGHT_GREY);
-		m_notebook1->AddPage(m_subPanel2, "Staff Profiles");
-
-	}
-	void UpdateDisplay(const std::vector<PatientProfile>& patient, const StaffProfile& staff, time_t currenttime) {
-		m_patientTot = "";
-		for (const auto& pat : patient){
-			wxString fullName = wxString::FromUTF8(pat.getFirstName() + " " + pat.getLastName());
-			wxString reason = wxString::FromUTF8(pat.getReasonOfAdmission());
-			m_patientTot += "Patient: " + fullName + "\nReason: " + reason + "\n";
-
-			//m_patientLabel->SetLabel(wxString::Format("Patient: %s\nReason: %s", fullName, reason));
-	}
-		m_patientLabel->SetLabel(m_patientTot);
-		m_staffShifts->SetLabel("Avtive Staff: " + wxString::FromUTF8(staff.getFirstName()));
-		this->Layout();
-	}
-	
-private:
-	wxBoxSizer* m_profSizer;
-	wxBoxSizer* m_patSizer;
-	wxBoxSizer* m_trSizer;
-
-	wxNotebook* m_notebook1;
-
-	wxPanel* m_subPanel1;
-	wxPanel* m_subPanel2;
-
-
-	wxString shiftDisplay = "Upcoming Shifts:\n";
-	wxString m_patientTot;
-
-	wxStaticText* m_staffShifts;
-	wxStaticText* m_patientLabel;
-
-
-
-};
+	time_t currentTime = wxDateTime::Now().GetTicks();
+	page2->UpdateDisplay(m_patient, *m_currentStaff, currentTime);
+	wxString timeStr = (wxDateTime::Now().Format("%Y, %B %d, %A - %H:%M:%S"));
+	SetStatusText(timeStr);
+	m_Index++;
+}
