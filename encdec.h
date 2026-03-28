215bafc54e7e6c520c684695e9e908ab100730fd
jjonn0 <87209376+jjonn0@users.noreply.github.com>
2026-03-28 13:52:08 -0700

    Patched encdec.h
    Patched an issue where storing the iv at the front of the ciphertext made for a very obvious vulnerability. iv is now dispersed throughout the ciphertext based on the password given.
    
    KNOWN ISSUES:
    - Entropy loss occurs when generating the indices to store the iv, as the algorithm to generate the indices only accounts for the length of the iv and ignores password length.
    - Entropy loss occurs when the input password does not equal or exceed the length of the key. This results in a significant number of null characters when the password is less than 16 characters long.

--- encdec.h
+++ encdec.h
@@ -10,6 +10,7 @@
 #pragma once
 #include <iostream>
 #include <array>
+#include <vector>
 #include <bitset>
 #include <format>
 
@@ -25,6 +26,7 @@
 
 constexpr size_t BYTE_SIZE{8};
 constexpr size_t HEXADECIMAL{16};
+constexpr size_t MAX_CHAR_VALUE{255};
 constexpr size_t KEY_LENGTH{CryptoPP::AES::DEFAULT_KEYLENGTH};
 
 // Set to true to enable debug mode. Prints results into terminal.
@@ -35,8 +37,39 @@ class EncDec
 	private:
 	CryptoPP::SecByteBlock m_iv;
 
+	// Generates first 256 prime numbers (excl. 1)
+	std::array<size_t, MAX_CHAR_VALUE + 1> generatePrimes()
+	{
+		std::array<size_t, MAX_CHAR_VALUE + 1> primes{};
+		size_t prime_count{ 0 };
+		size_t prime{ 0 };
+		while (prime_count < MAX_CHAR_VALUE + 1)
+		{
+			prime++;
+			size_t n{ 2 };
+			bool is_prime{ true };
+			while (n < prime)
+			{
+				if (prime % n == 0) {
+					is_prime = false;
+					break;
+				}
+				n++;
+			}
+			if (is_prime)
+			{
+				if (prime == 1) { continue; }
+				primes[prime_count] = prime;
+				prime_count++;
+			}
+		}
+		return primes;
+	};
+
+	/*******************************************************************************\
+	\*******************************************************************************/
+
 	public:
-	
 	explicit EncDec() {}
 
 	CryptoPP::SecByteBlock generateIV()
@@ -65,20 +98,15 @@ class EncDec
 			// Create a bit representation of the char
 			std::bitset<BYTE_SIZE> char_bits{ size_t(remaining_string_char) };
 
-			if (DEBUG_MODE) std::cout << std::format("Looking at char {} : [{}]\n", remaining_string_char, remaining_string_index);
-
+			//if (DEBUG_MODE) std::cout << std::format("Looking at char {} : [{}]\n", remaining_string_char, remaining_string_index);
 
 			// Keep looping until we have covered a full byte of data
 			do
 			{
 				size_t char_bit_index{ bit_index - ((remaining_string_index * BYTE_SIZE) % total_bits) };
-
-				if (DEBUG_MODE) std::cout << std::format("Accessing key index: {}, value: {}. Accessing char index: {}, value: {}. XOR result: ", bit_index, int(bits[bit_index]), char_bit_index, int(char_bits[char_bit_index]));
-
+				//if (DEBUG_MODE) std::cout << std::format("Accessing key index: {}, value: {}. Accessing char index: {}, value: {}. XOR result: ", bit_index, int(bits[bit_index]), char_bit_index, int(char_bits[char_bit_index]));
 				bits.set(bit_index, bits[bit_index] ^ char_bits[char_bit_index]);
-
-				if (DEBUG_MODE) std::cout << bits[bit_index] << std::endl;
-
+				//if (DEBUG_MODE) std::cout << bits[bit_index] << std::endl;
 				bit_index++;
 			} while (bit_index % BYTE_SIZE != 0);
 		}
@@ -88,7 +116,7 @@ class EncDec
 		return bits;
 	}
 
-	std::string bitsToString(const std::bitset<BYTE_SIZE* KEY_LENGTH> bits)
+	std::string bitsToString(const std::bitset<BYTE_SIZE* KEY_LENGTH>& bits)
 	{
 		std::string string_out{ "" };
 
@@ -107,13 +135,93 @@ class EncDec
 		return string_out;
 	}
 
-	CryptoPP::SecByteBlock stringToKey(std::string password)
+	CryptoPP::SecByteBlock stringToKey(const std::string& password)
 	{
 		// Truncate password
 		std::bitset<BYTE_SIZE* KEY_LENGTH> password_bits{generateBytes(password)};
 		std::string key_string{bitsToString(password_bits)};
 		CryptoPP::SecByteBlock key((const uint8_t*)(key_string.c_str()), KEY_LENGTH); // Pass in an unsigned char array pointer, of length KEY_LENGTH. Unsigned char just means a byte of data without a bit used for sign. A.K.A. a character.
 		return key;
+	}
+
+	/**
+	* @brief A function that returns a vector of positions genereted by a seed.
+	*/
+	std::vector<size_t> generateIndices(const std::string& seed, const size_t& data_length)
+	{
+		auto charValues([](const std::string& data, std::vector<size_t>& return_vector){
+			for(const char& c : data)
+			{
+				return_vector.push_back(size_t(c));
+			}
+			});
+
+		std::vector<size_t> seed_char_values{};
+		std::vector<size_t> return_vector{};
+		charValues(seed, seed_char_values);
+		std::array<size_t, MAX_CHAR_VALUE + 1> primes{generatePrimes()};
+		size_t seed_index{0};
+		for(size_t i{0}; i < data_length; i++)
+		{
+			size_t char_value{0}, prime_value{0};
+			char_value = seed_char_values[seed_index];
+			prime_value = primes[char_value];
+			return_vector.push_back(prime_value);
+			seed_index++;
+			if(seed_index == seed.length()) { seed_index = 0; }
+		}
+
+		return return_vector;
+	}
+
+	/**
+	* @brief A function that uses the password to determine where to slot in characters from the splice string into the host string. Used to hide iv.
+	*/
+	std::string spliceWithPassword(const std::string& password, const std::string& host_string, const std::string& splice_string)
+	{
+		std::vector<size_t> indices{generateIndices(password, splice_string.length())};
+		std::string return_string{host_string};
+		size_t next_index{0};
+		size_t splice_string_index{0};
+		while(!indices.empty())
+		{
+			next_index = indices.back();
+			next_index %= return_string.length();
+			indices.pop_back();
+
+			std::string front_string{return_string.substr(0, next_index)};
+			std::string back_string{return_string.substr(next_index, return_string.length() - next_index)};
+			return_string = "";
+			return_string += front_string + splice_string[splice_string_index] + back_string;
+			splice_string_index++;
+		}
+
+		return return_string;
+	}
+
+	/**
+	* @brief A function that undoes the spliceWithPassword function. Returns the value that was spliced into the host string. Alters input data string to revert.
+	*/
+	std::string combineFromPassword(const std::string& password, std::string& data, const size_t splice_string_length)
+	{
+		std::vector<size_t> indices{generateIndices(password, splice_string_length)};
+		std::string splice_string{""};
+		for(size_t i{0}; i < indices.size(); i++)
+		{
+			size_t index{indices[i]};
+			index %= data.length() - 1;
+			splice_string += data[index];
+			data.erase(index, 1);
+		}
+
+		std::string reversed_string{""};
+
+		for(int i{int(splice_string.length() - 1)}; i >= 0; --i)
+		{
+			reversed_string += splice_string[i];
+		}
+
+		return reversed_string;
 	}
 
 	std::string encrypt(const std::string& password, const std::string& plaintext)
@@ -133,8 +241,8 @@ class EncDec
 		std::string ciphertext{""};
 		CryptoPP::StringSource ss(plaintext, true, new CryptoPP::AuthenticatedEncryptionFilter(encryptor, new CryptoPP::StringSink(ciphertext)));
 
-		// "Tamper"
-		//ciphertext[0] ^= 0x01;
+		// Tamper
+		ciphertext[0] ^= 0x01;
 
 		std::string cipher_string{""};
 		CryptoPP::StringSource rs(ciphertext, true, new CryptoPP::HexEncoder(new CryptoPP::StringSink(cipher_string)));
@@ -144,7 +252,7 @@ class EncDec
 		iv_enc.Put(m_iv, m_iv.size());
 
 		std::string return_string{""};
-		return_string += iv_string + cipher_string;
+		return_string = spliceWithPassword(password, cipher_string, iv_string);
 
 		if(DEBUG_MODE)
 		{
@@ -163,18 +271,19 @@ class EncDec
 
 		CryptoPP::SecByteBlock key{stringToKey(password)};
 
+		std::string ciphertext_hex{ciphertext};
 		// Retrieve the IV from the ciphertext. Every char is represented as two hexadecimal values.
-		std::string iv_hex{ciphertext.substr(0, HEXADECIMAL * 2)};
+		std::string iv_hex{combineFromPassword(password, ciphertext_hex, HEXADECIMAL * 2)};
 		std::string iv_str{""};
 		CryptoPP::StringSource ss_iv(iv_hex, true, new CryptoPP::HexDecoder(new CryptoPP::StringSink(iv_str)));
 		CryptoPP::SecByteBlock iv((const uint8_t*)(iv_str.c_str()), KEY_LENGTH);
 
 		CryptoPP::EAX<CryptoPP::AES>::Decryption decryptor;
 		decryptor.SetKeyWithIV(key, key.size(), iv, iv.size());
-
-		std::string message_hex{ ciphertext.substr(iv_hex.length(), ciphertext.length()) };
 		std::string encrypted_message{""};
-		CryptoPP::StringSource ss_msg(message_hex, true, new CryptoPP::HexDecoder(new CryptoPP::StringSink(encrypted_message)));
+		CryptoPP::StringSource ss_msg(ciphertext_hex, true, new CryptoPP::HexDecoder(new CryptoPP::StringSink(encrypted_message)));
+
+		encrypted_message[0] ^= 0x01;
 		
 		std::string plaintext{""};
 		CryptoPP::StringSource ss(encrypted_message, true, new CryptoPP::AuthenticatedDecryptionFilter(decryptor, new CryptoPP::StringSink(plaintext)));
