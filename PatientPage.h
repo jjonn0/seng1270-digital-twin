eb8df8dc779aa31ff67320aa661dce8f0296c809
zack337 <76924414+zack337@users.noreply.github.com>
2026-03-30 23:01:49 -0700

    Add PatientPage.h header file

---
+++ PatientPage.h
@@ -0,0 +1,137 @@
+#pragma once
+#include<wx/wx.h>
+#include<vector>
+#include<string>
+#include<format>
+
+#include <wx/notebook.h>
+#include <wx/simplebook.h>
+#include "simulation.h"
+#include "profiles.h"
+#include "StaffPage.h"
+
+
+class MainFrame;
+class PatientPage : public wxPanel{
+public:
+
+
+	PatientPage(wxWindow* Parent, MainFrame* frame) : wxPanel(Parent), m_frame1(frame) {
+		InitComps();
+		Styles();
+	};
+	void InitComps() {
+
+		m_patSizer = new wxBoxSizer(wxVERTICAL);
+		m_profSizer = new wxBoxSizer(wxVERTICAL);
+		m_scrollSizer = new wxBoxSizer(wxVERTICAL);
+
+		m_notebook1 = new wxNotebook(this, wxID_ANY, wxPoint(200, 50), wxSize(400, 300));
+
+		m_patPage = new wxPanel(m_notebook1);
+		m_staffPage = new StaffPage(m_notebook1, static_cast<wxSimplebook*>(this->GetParent()), m_frame1);
+
+		m_scrolledWindow1 = new wxScrolledWindow(m_patPage, wxID_ANY);
+		m_scrolledWindow1->SetScrollRate(0, 10);
+
+		//m_staffShifts = new wxStaticText(m_staffPage, wxID_ANY, "Loading...");
+		m_patientLabel = new wxStaticText(m_scrolledWindow1, wxID_ANY, "Loading...");
+		m_addFirst = new wxTextCtrl(m_patPage, wxID_ANY, "");
+		m_addLast = new wxTextCtrl(m_patPage, wxID_ANY, "");
+		m_addReason = new wxTextCtrl(m_patPage, wxID_ANY, "");
+		m_addAge= new wxTextCtrl(m_patPage, wxID_ANY, "");
+
+		homeBtn = new wxButton(m_patPage, wxID_ANY, "Home");
+		admitBtn = new wxButton(m_patPage, wxID_ANY, "Submit");
+
+			
+	
+
+
+
+		}
+	void Styles() {
+
+		this->SetBackgroundColour(*wxLIGHT_GREY);
+		m_patPage->SetBackgroundColour(*wxLIGHT_GREY);
+
+		m_notebook1->AddPage(m_patPage, "Patient Profiles");
+
+		m_staffPage->SetBackgroundColour(*wxLIGHT_GREY);
+		m_notebook1->AddPage(m_staffPage, "Staff Profiles");
+
+
+		m_addFirst->SetHint("Enter First");
+		m_addLast->SetHint("Enter Last");
+		m_addReason->SetHint("Enter Reason");
+		m_addAge->SetHint("Enter DOB ex: 2026, 12, 24");
+
+		homeBtn->Bind(wxEVT_BUTTON, [=](wxCommandEvent&) { static_cast<wxSimplebook*>(this->GetParent())->SetSelection(0); });
+		admitBtn->Bind(wxEVT_BUTTON, &PatientPage::OnAdmit, this);
+		m_scrollSizer->Add(m_patientLabel, 1, wxEXPAND | wxALL, 10);
+		m_scrolledWindow1->SetSizer(m_scrollSizer);
+
+		m_patSizer->Add(homeBtn, 0, wxALIGN_CENTER_HORIZONTAL | wxTOP, 0);
+		m_patSizer->Add(m_scrolledWindow1, 1, wxEXPAND | wxLEFT | wxRIGHT, 10);
+		m_patSizer->Add(admitBtn, 0, wxALIGN_CENTER_HORIZONTAL, 20);
+		m_patSizer->Add(m_addFirst, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
+		m_patSizer->Add(m_addLast, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
+		m_patSizer->Add(m_addReason, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
+		m_patSizer->Add(m_addAge, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
+
+		m_patPage->SetSizer(m_patSizer);
+		m_profSizer->Add(m_notebook1, 1, wxEXPAND, 0);
+		this->SetSizer(m_profSizer);
+
+		}
+	void UpdateDisplay(const std::vector<PatientProfile>&patient, const std::vector<StaffProfile>& staff, time_t currenttime) {
+		m_patientTot = "";
+		m_staffPage->UpdateDisplay(staff, currenttime);
+		for (const auto& pat : patient) {
+			wxDateTime dob(pat.getDOB());
+			wxDateTime now = wxDateTime::Now();
+			int age = now.GetYear() - dob.GetYear();
+			wxString fullName = wxString::FromUTF8(pat.getFirstName() + " " + pat.getLastName());
+			wxString reason = wxString::FromUTF8(pat.getReasonOfAdmission());
+			m_patientTot += wxString::Format("Patient: %s\nAge: %d\nReason %s\nID: %s\n\n", fullName, age ,reason, pat.getProfileNumber());
+
+			}
+		m_patientLabel->SetLabel(m_patientTot);
+		m_patientLabel->Wrap(m_scrolledWindow1->GetClientSize().GetWidth());
+		m_scrolledWindow1->FitInside();
+		m_scrolledWindow1->Scroll(0, m_scrolledWindow1->GetScrollLines(wxVERTICAL));
+		wxDateTime now(currenttime);
+		//m_staffShifts->SetLabel("Active Staff: " + wxString::FromUTF8(staff.getFirstName()));
+		this->Layout();
+	}
+		void OnAdmit(wxCommandEvent& evt);
+private:
+	wxBoxSizer* m_profSizer;
+	wxBoxSizer* m_patSizer;
+	wxBoxSizer* m_scrollSizer;
+
+	wxNotebook* m_notebook1;
+	wxScrolledWindow* m_scrolledWindow1;
+
+	wxPanel* m_patPage;
+	StaffPage* m_staffPage;
+
+	wxTextCtrl* m_addFirst;
+	wxTextCtrl* m_addLast;
+	wxTextCtrl* m_addReason;
+	wxTextCtrl* m_addAge;
+
+
+
+	wxString shiftDisplay = "Upcoming Shifts:\n";
+	wxString m_patientTot;
+
+	wxStaticText* m_staffShifts;
+	wxStaticText* m_patientLabel;
+
+	wxButton* homeBtn;
+	wxButton* admitBtn;
+
+	MainFrame* m_frame1;
+
+	};
