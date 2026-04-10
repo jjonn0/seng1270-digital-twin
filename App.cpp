ddf7014706d03f6b7dc015a24307cf644fe3599a
zack337 <76924414+zack337@users.noreply.github.com>
2026-04-09 18:11:09 -0700

    Implement password entry dialog on app initialization
    Added password protection to application startup. Currently only pre-set customizable password coming soon

--- App.cpp
+++ App.cpp
@@ -5,10 +5,29 @@
 
 wxIMPLEMENT_APP(App);
 
-bool App::OnInit()
+bool App::OnInit() 
 {
-	MainFrame* mainFrame = new MainFrame("VitalVitals");
-	mainFrame->SetClientSize(1000, 600);
+	wxString password = "Secret";
+	bool passwordCorrect = false;
+	while (!passwordCorrect) {
+		wxPasswordEntryDialog login(nullptr, "Enter System Password:", "VitalVitals Login");
+
+		if (login.ShowModal() == wxID_OK) {
+			password = login.GetValue();
+			if (password == "Secret") {
+				passwordCorrect = true;
+			}
+			else {
+				wxMessageBox("Incorrect password. Please try again.", "Login Failed", wxICON_ERROR);
+			}
+		}
+		else {
+			return false; // User cancelled the login dialog
+		}
+	}
+
+	MainFrame* mainFrame = new MainFrame("VitalVitals GUI", password.ToStdString());
+	mainFrame->SetClientSize(800, 600);
 	mainFrame->Center();
 	mainFrame->Show();
 	return true;
