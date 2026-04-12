f0f13e3d5f5d03488a99379480eac91b51045da9
Ranshika Fernandopulle <shikapulle@gmail.com>
2026-04-11 19:19:45 -0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QString>
+#include <QDate>
+#include <QTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,10 +21,52 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-protected:
-    void paintEvent(QPaintEvent *event) override;
+    struct PatientRecord {
+        int id;
+        QString firstName;
+        QString lastName;
+        QString dob;
+        QString sex;
+        QString reason;
+        QString timeOfAdmission;
+        QString expectedStay;
+        QString unit;
+    };
+
+    struct ShiftRecord {
+        QDate date;
+        QTime startTime;
+        QTime endTime;
+    };
+
+    struct StaffRecord {
+        int id;
+        QString firstName;
+        QString lastName;
+        QString role;
+        QString assignedUnit;
+        QVector<ShiftRecord> shifts;
+    };
 
 private:
     Ui::MainWindow *ui;
+
+    QVector<PatientRecord> allPatients;
+    QVector<PatientRecord> currentPatients;
+    QString predictExpectedStay(const QString &unit);
+
+    QVector<StaffRecord> allStaff;
+    int currentStaffIndex = -1;
+
+    void loadSamplePatients();
+    void showPatientsForUnit(const QString &unit);
+    void showPatientDetails(int index);
+    void addPatientFromInput();
+
+    void loadSampleStaff();
+    void showStaffDetails(int index);
+    void addShiftToStaff();
+    int selectedMonthNumber() const;
 };
+
 #endif // MAINWINDOW_H
