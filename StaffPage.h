69c9093c64a605e8f6b8df55b612a4315c9eec4c
zack337 <76924414+zack337@users.noreply.github.com>
2026-04-12 18:24:52 -0700

    Modify age hint and enhance comments in StaffPage.h
    Updated hint text for age input and improved comments for clarity.

--- StaffPage.h
+++ StaffPage.h
@@ -31,17 +31,21 @@ public:
 		m_addJob = new wxTextCtrl(this, wxID_ANY, "");
 		m_addAge = new wxTextCtrl(this, wxID_ANY, "");
 		m_stafftext = new wxStaticText(m_scrollWindow2, wxID_ANY, "");
+		// Initializes the components of the staff page, which includes a home button that takes the user back to the home page, an admit button that will allow the user to add new staff members, and text controls for entering the first name, last name, job title, and age of the new staff member. A scrolled window is also created to display the list of staff members, and a static text is initialized to show the staff information.
+		
 
 		m_addFirst->SetHint("First Name");
 		m_addLast->SetHint("Last Name");
 		m_addJob->SetHint("Job Title");
-		m_addAge->SetHint("Age");
+		m_addAge->SetHint("Enter DOB ex: 2026, 12, 24");
+		// Sets the hint text for the text controls, which provides guidance to the user on what information to enter in each field. The first name, last name, job title, and date of birth fields are all given appropriate hints to help the user understand what information is required for admitting a new staff member.
 
 		wxArrayString unitOptions;
 		unitOptions.Add("GENERAL");  unitOptions.Add("ER"); unitOptions.Add("ICU"); unitOptions.Add("CARDIAC"); unitOptions.Add("NEURO"); unitOptions.Add("POST_OP");
 		unitOptions.Add("GERIATRIC"); unitOptions.Add("TRAUMA"); unitOptions.Add("NEPHRO"); unitOptions.Add("PSHYC"); unitOptions.Add("ONCOLOGY"); unitOptions.Add("PALLIATIVE");
-		//m_unitChoice = new wxChoice(m_patPage, wxID_ANY, wxDefaultPosition, wxDefaultSize, unitOptions);
 		this->SetSizer(m_staffSizer);
+		// Creates an array of strings representing different hospital units, which can be used for categorizing staff members based on their job roles. The options include various departments such as General, ER, ICU, Cardiac, Neuro, Post-Op, Geriatric, Trauma, Nephrology, Psychiatry, Oncology, and Palliative care. The staff page's sizer is set to the vertical box sizer initialized earlier to manage the layout of the components on the page.
+		
 	}
 	void Styles() {
 		this->SetBackgroundColour(*wxLIGHT_GREY);
@@ -61,18 +65,20 @@ public:
 		m_staffSizer->Add(m_addJob, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
 		m_staffSizer->Add(m_addAge, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
 		this->Layout();
+		//Adds all the components to the sizers and sets up the layout of the staff page.		
 	}
 
 	void UpdateDisplay(const std::vector<StaffProfile>& stafflist, wxDateTime currenttime) {
 		m_staffTot = "";
 		for (const auto& staff : stafflist) {
 
-			//m_staffTot += staff.getFirstName() + " " + staff.getLastName() + "\n";
 			wxDateTime dob(staff.getDOB());
 			wxDateTime now(currenttime);
 			int age = now.GetYear() - dob.GetYear();
 			m_fullName = staff.getFirstName() + " " + staff.getLastName();
-			m_staffTot += wxString::Format("Name: %s\nAge: %d\nOccupation: %s\nID: %s\n\n", m_fullName, age, staff.getOccupation(), staff.getProfileNumber());
+			m_staffTot += wxString::Format("Name: %s\nAge: %d\nOccupation: %s\nID: %llu\n\n", m_fullName, age, staff.getOccupation(), staff.getProfileNumber());
+			
+			
 		}
 		m_stafftext->SetLabel(m_staffTot);
 		m_stafftext->Wrap(m_scrollWindow2->GetClientSize().GetWidth());
@@ -80,6 +86,7 @@ public:
 		m_scrollWindow2->Scroll(0, m_scrollWindow2->GetScrollLines(wxVERTICAL));
 		m_scrollSizer->Layout();
 		this->Layout();
+		//Puts all new informations of the staff members into a string and then sets that onto the scroll window.
 	}
 	void OnPress(wxCommandEvent& evt);
 private:
@@ -100,10 +107,7 @@ private:
 	wxString m_staffTot;
 	wxString m_fullName;
 
-
 	wxButton* homeBtn;
 	wxButton* admitBtn;
 	MainFrame* m_frame2;
-
-
 };
